a8b73de24c2a1a9301b34d460ceebf2f457fd6a6
Vojislav Gavrilov <vojislav.gavrilov@gmail.com>
2015-12-30 13:17:40 +0100

    First commit

---
+++ pieceslist.h
@@ -0,0 +1,23 @@
+#ifndef PIECESLIST_H
+#define PIECESLIST_H
+
+#include <QListWidget>
+
+class PiecesList : public QListWidget
+{
+    Q_OBJECT
+
+public:
+    explicit PiecesList(int pieceSize, QWidget *parent = 0);
+    void addPiece(QPixmap pixmap, QPoint location);
+
+protected:
+    void dragEnterEvent(QDragEnterEvent *event) Q_DECL_OVERRIDE;
+    void dragMoveEvent(QDragMoveEvent *event) Q_DECL_OVERRIDE;
+    void dropEvent(QDropEvent *event) Q_DECL_OVERRIDE;
+    void startDrag(Qt::DropActions supportedActions) Q_DECL_OVERRIDE;
+
+    int m_PieceSize;
+};
+
+#endif // PIECESLIST_H
